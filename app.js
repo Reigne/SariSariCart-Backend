0eb6f5fe50821f1bb0e030a6b43221af5471032e
Reigne <monteronaelija@yahoo.com>
2024-07-13 23:13:13 +0800

    change cors

--- app.js
+++ app.js
@@ -9,16 +9,32 @@ const users = require("./routes/user");
 
 var cors = require("cors");
 
-app.use(
-  cors({
-    origin: `${process.env.FRONTEND_URL}`,
-    credentials: true,
-  })
-);
+const allowedOrigins = [`${process.env.FRONTEND_URL}`];
 
 app.use(cookieParser());
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ limit: "100mb", extended: true }));
+
+app.use(cors({
+  origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+          callback(null, true);
+      } else {
+          callback(new Error('Not allowed by CORS'));
+      }
+  },
+  credentials: true,
+}));
+
+
+// app.use(
+//   cors({
+//     origin: `${process.env.FRONTEND_URL}`,
+//     credentials: true,
+//   })
+// );
+
+
 
 app.use("/api/v1", categories);
 app.use("/api/v1", products);
