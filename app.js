fec47143a6c36eeba73f336dcf244f62f53aae73
Reigne <monteronaelija@yahoo.com>
2024-09-08 23:49:04 +0800

    added url in allowedOrigins

--- app.js
+++ app.js
@@ -7,21 +7,25 @@ const errorMiddleware = require("./middlewares/errors");
 const categories = require("./routes/category");
 const products = require("./routes/product");
 const users = require("./routes/user");
+const orders = require("./routes/order");
 
-const allowedOrigins = [`${process.env.FRONTEND_URL}`];
+const allowedOrigins = [`http://localhost:3000`, `${process.env.FRONTEND_URL}`];
 
 app.use(cookieParser());
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ limit: "100mb", extended: true }));
 
-app.use(cors({
-  origin: allowedOrigins,
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
 
 app.use("/api/v1", categories);
 app.use("/api/v1", products);
 app.use("/api/v1", users);
+app.use("/api/v1", orders);
 
 app.use(errorMiddleware);
 
