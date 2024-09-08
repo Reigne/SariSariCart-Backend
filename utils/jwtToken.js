5285ebdf1b65193b59a4473cbd1ba13c34842164
Reigne <monteronaelija@yahoo.com>
2024-09-09 00:22:53 +0800

    removed options in return

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,34 +1,25 @@
 const sendToken = (user, statusCode, res) => {
-  // Create Jwt token
-  const token = user.getJwtToken();
-
-  // Options for cookie
-  const options = {
-    expires: new Date(
-      Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
-    ),
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-  };
-
-  console.log(token, "token");
-
-  res
-    .status(statusCode)
-    .cookie("token", token, {
+    // Create Jwt token
+    const token = user.getJwtToken();
+  
+    // Options for cookie
+    const options = {
       expires: new Date(
         Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
       ),
-      httpOnly: true, // Cookie can't be accessed via client-side JavaScript
-      secure: process.env.NODE_ENV === "production", // Cookie sent only over HTTPS in production
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // For cross-site cookie policy
-    })
-    .json({
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax"
+    }
+  
+    console.log(token, "token")
+  
+    res.status(statusCode).cookie('token', token, options).json({
       success: true,
       token,
-      user,
+      user
     });
-};
-
-module.exports = sendToken;
+  }
+  
+  module.exports = sendToken;
+  
